fa37c35da4a51ebbf4a244b2e9a7db4747df56ec
Your Name Comes Here <you@yourdomain.example.com>
2021-06-09 09:30:42 -0700

    add index.py

---
+++ index.py
@@ -0,0 +1,43 @@
+import random
+print("This is a dice stimulator")
+x = "y"
+
+while x == "y":
+    number = random.randint(1,6)
+    if number == 1:
+        print("-----------")
+        print("|         |")
+        print("|    o    |")
+        print("|         |")
+        print("-----------")
+    if number == 2:
+        print("-----------")
+        print("|    o    |")
+        print("|         |")
+        print("|    o    |")
+        print("-----------")
+    if number == 3:
+        print("-----------")
+        print("|    o    |")
+        print("|    o    |")
+        print("|    o    |")
+        print("-----------") 
+    if number == 4:
+        print("-----------")
+        print("|  o   o  |")
+        print("|         |")
+        print("|  o   o  |")
+        print("-----------")  
+    if number == 5:
+        print("-----------")
+        print("|  o   o  |")
+        print("|    o    |")
+        print("|  o   o  |")
+        print("-----------")
+    if number == 6:
+        print("-----------")
+        print("|  o   o  |")
+        print("|  o   o  |")
+        print("|  o   o  |")
+        print("-----------")
+    x = input.lower("Press y to roll again:")
